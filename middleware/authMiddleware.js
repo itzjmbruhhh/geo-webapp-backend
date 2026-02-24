3f3636802597792227af78eac832ca3cdacbf40a
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2026-02-25 00:28:00 +0800

    added initial middleware/authMiddleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'No token' });
+
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+}
